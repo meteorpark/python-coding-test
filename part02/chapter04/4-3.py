a90879400413bef85975f9946fbad865745a550c
Matt Park <matt.park@gbike.io>
2021-02-26 09:38:14 +0900

    4-3은 진행 중

---
+++ part02/chapter04/4-3.py
@@ -0,0 +1,31 @@
+# 8x8 좌표평면
+# 행 (a ~ h)
+# 열 (1 ~ 8)
+# 아래 오른쪽 값은 양수, 위쪽 왼쪽은 음수
+# 나이트의 움직임은 두 가지 이다
+# 1. 수평으로 두 칸 이동한 뒤에 수직으로 한 칸 이동하기
+# 2. 수직으로 두 칸 이동한 뒤에 수평으로 한 칸 이동하기
+
+
+# 현재 나이트의 위치 입력받기
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+
+# 나이트가 이동할 수 있는 8가지 방향 정의
+
+steps = [
+    (-2, -1), (-1, -2), (1, -2), (2, -1),
+    (2, 1), (1, 2), (-1, 2), (-2, 1)
+]
+
+result = 0
+for step in steps:
+    # 이동하고자 하는 위치 확인
+    next_row = row + step[0]
+    next_column = column + step[1]
+    # 해당 위치로 이동이 가능하다면 카운트 증가
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
+
+print(result)
